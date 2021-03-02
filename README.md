9736b119cbcc904cbb2f1daef5bcef486631f51c
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-03-02 01:36:19 -0500

    finished lab 11

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # book_app
+
+## User Stories 
+
+1. As a user, I want my application to load quickly so that I have an enjoyable experience.
+2. As a user, I want to search the Google Books API so that I can view the results of my search.
+3. As a user, I want to be able to browse the search results.
+4. As a user, I want to view any error messages that occur during the usage of my book list application so that I know if something has gone wrong.
+5. As a user, I want a simple, clean looking UI so that my application is easy to navigate.
--- server.js
+++ server.js
@@ -1,23 +1,61 @@
 'use strict'
 
+// ============== Packages ==============================
 const express = require('express');
 const superagent = require('superagent');
 require('dotenv').config();
 
-const app = express();
 
+// ============== App ===================================
+const app = express();
 const PORT = process.env.PORT || 3003;
 
+// EJS requires us to have a folder called views where the ejs lives
 app.set('view engine', 'ejs');
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended:true}));
 
 
-app.get('/hello', (req, res) => {
+// ============== Routes ================================
+app.get('/search', renderSearch);
+app.post('/searches', getBooksCallback)
+
+function getBooksCallback(req, res){
+  let userChoice = req.body.search[1]
+  let url = 'https://www.googleapis.com/books/v1/volumes?q='
+  if (userChoice === 'author'){
+    url += `+inauthor:${req.body.search[0]}`
+  }
+  if (userChoice === 'title'){
+    url += `+intitle:${req.body.search[0]}`
+  }
+  superagent.get(url)
+    .then(bookInfo => {
+      const output = bookInfo.body.items.map(book => new Book(book))
+      res.render('pages/searches/show', {output: output});
+    })
+    .catch(error =>{
+      console.log(error);
+      res.status(500).send(`Sorry something went wrong`);
+    });
+
+}
+
+function renderSearch(req, res) {
+  res.render('pages/searches/new.ejs')
+}
+
+
+app.get('/', (req, res) => {
   res.render('pages/index')
 })
 
+function Book(object){
+  this.image = object.volumeInfo.imageLinks ? object.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg'
+  this.title = object.volumeInfo.title;
+  this.author = object.volumeInfo.authors[0]
+  this.description = object.volumeInfo.description
+}
 
-
-
+// ============== Initialization ========================
 app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`));
